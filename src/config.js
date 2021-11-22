8364c7884954ea0493b19b74f7070ad936e9aa22
abdulwahed-mansour <abdulwahed.mansour@gmail.com.com>
2021-11-22 18:54:05 +0200

    from me

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
 const layersOrder = [
-    { name: 'background', number: 1 },
-    { name: 'ball', number: 2 },
-    { name: 'eye color', number: 12 },
-    { name: 'iris', number: 3 },
-    { name: 'shine', number: 1 },
-    { name: 'shine', number: 1 },
-    { name: 'bottom lid', number: 3 },
-    { name: 'top lid', number: 3 },
+    { name: 'Background', number: 4 },
+    { name: 'Head', number: 3 },
+    { name: 'Face', number: 3 },
 ];
   
 const format = {
@@ -20,6 +15,6 @@ const rarity = [
     { key: "_sr", val: "super rare" },
 ];
 
-const defaultEdition = 5;
+const defaultEdition = 50;
 
 module.exports = { layersOrder, format, rarity, defaultEdition };
